14b2114ce47cf79051fb02c30b80af6000074512
= <benjamin.mchone@gmail.com>
2015-01-22 12:00:25 -0600

    added all files (again)

---
+++ desktop/src/com/cards/framework/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cards.framework.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cards.framework.CardGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CardGame(), config);
+	}
+}
