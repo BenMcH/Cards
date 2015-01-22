1e2ed93e269c651580b6e6b850a51a7dda5c54c4
= <benjamin.mchone@gmail.com>
2015-01-22 12:53:13 -0600

    Added the game state manager as well as the play state.

---
+++ core/src/com/cards/framework/gamestates/PlayState.java
@@ -0,0 +1,31 @@
+package com.cards.framework.gamestates;
+
+import com.cards.framework.managers.GameStateManager;
+
+public class PlayState extends GameState {
+
+	public PlayState(GameStateManager gsm) {
+		super(gsm);
+	}
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void handleInput() {
+	}
+
+	@Override
+	public void update(float deltaTime) {
+	}
+
+	@Override
+	public void draw() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
